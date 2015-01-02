6ee06ba714a1c1545977ffb36aec66047fc1c4bc
ele90atza <ele.atza@gmail.com>
2015-01-02 23:48:59 +0100

    Test coordinate con network provider

--- myweathernow/src/com/example/myweathernow/MWNhome.java
+++ myweathernow/src/com/example/myweathernow/MWNhome.java
@@ -1,6 +1,10 @@
 package com.example.myweathernow;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MWNhome extends Activity {
@@ -11,5 +15,31 @@ public class MWNhome extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        Location last = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        System.out.println(last.getLatitude());
+        System.out.println(last.getLongitude());
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                System.out.println("NUOVA LOCATION");
+                System.out.println(location.getLatitude());
+                System.out.println(location.getLongitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, locationListener);
     }
 }
