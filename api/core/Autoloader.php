ab68c673c509ea97057d4cdae82bdbbad3610ebc
Roberto Tucci <tucciroberto2000@gmail.com>
2015-01-04 16:38:59 +0100

    created api section. created get register and get endpoint.

---
+++ api/core/Autoloader.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roberto
+ * Date: 28/06/14
+ * Time: 3.04
+ */
+
+namespace core;
+
+class Autoloader {
+
+    public function __construct(){
+        set_include_path(dirname(__FILE__));
+        spl_autoload_register(array($this, 'importClass'));
+    }
+
+    private function importClass($className){
+        $namespaceSeparator = "\\";
+
+        $className          = ltrim($className, $namespaceSeparator);
+
+        $fileName = str_replace($namespaceSeparator, DIRECTORY_SEPARATOR, $className);
+
+        include $fileName.".php";
+    }
+}
+
+new AutoLoader();
