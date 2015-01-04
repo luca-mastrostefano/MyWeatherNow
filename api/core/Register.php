8a25fa70239bfcfaf60f14b1ae36f9cecf3a2759
Roberto Tucci <tucciroberto2000@gmail.com>
2015-01-04 17:53:54 +0100

    [api] removed register endpoint. Implemented call to external forecast data provider

--- api/core/Register.php
+++ api/core/Register.php
@@ -9,43 +9,15 @@
 namespace core;
 
 class Register {
-
-    private $email;
-
-    private $username;
-
     private $response;
 
     public function __construct() {
-        $filterArgs = array(
-            'email' => array( 'filter' => FILTER_SANITIZE_EMAIL ),
-            'username' => array( 'filter' => FILTER_SANITIZE_STRING,
-                                 'flags' => array( FILTER_FLAG_STRIP_HIGH, FILTER_FLAG_STRIP_LOW ) )
-        );
-
-        $postInput = filter_input_array( INPUT_POST, $filterArgs );
-
-        $this->email    = $postInput[ 'email' ];
-        $this->username = $postInput[ 'username' ];
         $this->response = array(
-            'status' => 200,
             'id' => -1
         );
-
-        if( empty($this->email) || !preg_match("/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/", $this->email)){
-            $this->response['status'] = 400;
-        }
-
-
     }
 
     public function doAction(){
-        if($this->response['status'] !== 200){
-            return;
-        }
-
-        //save <email-username> pair
-
         //return the unique ID
         $this->response['id'] = 1;
     }
