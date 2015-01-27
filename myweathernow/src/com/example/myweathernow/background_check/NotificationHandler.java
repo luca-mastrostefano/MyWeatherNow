e5ad4f8a367fea4d14b6299ddde2c297a69f9b40
ele90atza <ele.atza@gmail.com>
2015-01-27 20:32:35 +0100

    creato phrase handler per generare le frasi in base alla probabilità di pioggia

--- myweathernow/src/com/example/myweathernow/background_check/NotificationHandler.java
+++ myweathernow/src/com/example/myweathernow/background_check/NotificationHandler.java
@@ -5,7 +5,7 @@ import android.content.*;
 import android.support.v4.app.*;
 import com.example.myweathernow.*;
 import com.example.myweathernow.persistency.WeatherManager;
-import com.example.myweathernow.util.WeatherInfo;
+import com.example.myweathernow.util.*;
 
 /**
  * Created by lucamastrostefano on 04/01/15.
@@ -25,9 +25,10 @@ public class NotificationHandler {
 
     public void show(WeatherManager weatherManager) {
         if(weatherManager != null) {
+            String suggestion = PhraseMaker.getPhrase(weatherManager, APIManager.Day.TODAY);
             mBuilder.setSmallIcon(R.drawable.weather_sample);
 //        mBuilder.setContentText(weatherManager.getSentence());
-            mBuilder.setContentText(weatherManager.getOverview(APIManager.Day.TODAY).get(WeatherInfo.Period.DAILY).getSentence());
+            mBuilder.setContentText(suggestion);
             Intent openAppIntent = new Intent(this.c, MWNhome.class);
             PendingIntent openAppPendingIntent = PendingIntent.getActivity(this.c, 0, openAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             mBuilder.setContentIntent(openAppPendingIntent);
